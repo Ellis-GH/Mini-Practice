171492ebce4e780c978f53ab4181d3342e7b1e42
Ellis <ellison.linsley@gmail.com>
2025-11-26 07:58:54 +0000

    Added a SpriteRotator script, and put it on the player

--- Assets/PlayerMovementScript.cs
+++ Assets/PlayerMovementScript.cs
@@ -2,11 +2,14 @@ using UnityEngine;
 
 public class PlayerMovementScript : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private SpriteRotationScript spriteRotator;
+
     private InputSystem_Actions controls;
     private Vector2 moveInput;
 
     [SerializeField] float movementSpeed = 5f;
-    private Rigidbody2D rb;
+    
 
     private void Awake()
     {
@@ -17,6 +20,7 @@ public class PlayerMovementScript : MonoBehaviour
         controls.Player.Move.canceled += ctx => moveInput = Vector2.zero;
 
         rb = GetComponent<Rigidbody2D>();
+        spriteRotator = GetComponent<SpriteRotationScript>();
     }
     private void OnEnable()
     {
@@ -30,6 +34,9 @@ public class PlayerMovementScript : MonoBehaviour
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + moveInput * movementSpeed * Time.deltaTime);
+        if(moveInput.magnitude != 0) { spriteRotator.SetOrientation(moveInput); }
+
+        Debug.Log("Move input is: " + moveInput);
         //Time.deltaTime is a small number, like 0.01, but is tied to framerate, or the change in time between now and the last frame.
         //If not multiplied in, the player will move at [movementSpeed] every frame, which means they move slower at a slow framerate.
     }
---
+++ Assets/SpriteRotationScript.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class SpriteRotationScript : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+
+    [SerializeField] Sprite[] sprites;
+
+    public enum Direction
+    {
+        Up = 0,
+        Down = 2, 
+        Left = 1, 
+        Right = 2,
+    } // ORDER: UpLeft, UpRight, DownLeft, DownRight
+    private Direction horizontalDirection; //L/R
+    private Direction verticalDirection; //U/D
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void SetOrientation(Vector2 moveInput)
+    {
+        if(moveInput.x < 0) { horizontalDirection = Direction.Left; }
+        else if(moveInput.x > 0) { horizontalDirection = Direction.Right; }
+        if (moveInput.y < 0) { verticalDirection = Direction.Down; }
+        else if (moveInput.y > 0) { verticalDirection = Direction.Up; }
+
+        spriteRenderer.sprite = sprites[(int)horizontalDirection + (int)verticalDirection];
+    }
+}
