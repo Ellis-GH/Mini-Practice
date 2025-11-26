76cd673b8f2fdc44b0aae139ff3065c2ee0c1a1e
Ellis <ellison.linsley@gmail.com>
2025-11-26 08:04:10 +0000

    Hooked up sprites to the player, fixed a bug

--- Assets/SpriteRotationScript.cs
+++ Assets/SpriteRotationScript.cs
@@ -13,8 +13,8 @@ public class SpriteRotationScript : MonoBehaviour
         Left = 1, 
         Right = 2,
     } // ORDER: UpLeft, UpRight, DownLeft, DownRight
-    private Direction horizontalDirection; //L/R
-    private Direction verticalDirection; //U/D
+    private Direction horizontalDirection = Direction.Left; //L/R
+    private Direction verticalDirection = Direction.Down; //U/D
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -29,6 +29,6 @@ public class SpriteRotationScript : MonoBehaviour
         if (moveInput.y < 0) { verticalDirection = Direction.Down; }
         else if (moveInput.y > 0) { verticalDirection = Direction.Up; }
 
-        spriteRenderer.sprite = sprites[(int)horizontalDirection + (int)verticalDirection];
+        spriteRenderer.sprite = sprites[((int)horizontalDirection + (int)verticalDirection) - 1]; //-1 bc array is 0-3, not 1-4
     }
 }
